2884e00c87c6b43cd49c172f9c6c96ab8d93aba6
aviola98 <viola1@usp.br>
2021-06-20 19:00:53 -0300

    lista2

---
+++ lista_2.R
@@ -0,0 +1,41 @@
+#Lista 2
+
+local ({
+  .Table <- data.frame(Probability=dbinom(0:3, size=3,prob=0.5))
+  rownames(.Table) <- 0:3
+  print
+})
+
+#opening data
+library(tidyverse)
+library(knitr)
+library(readODS)
+library(ggplot2)
+
+banco_tempo <- read_ods("banco_tempo.ods")
+View(banco_tempo)
+
+#calculating descriptive statistics for the variable time
+
+estatisticas_descritivas_tempo <- banco_tempo %>%
+  summarize(mean_time=mean(Tempo),
+            median_time=median(Tempo),
+            sd_time=sd(Tempo),
+            max_time=max(Tempo),
+            min_time=min(Tempo)) %>%
+  kable(caption="Estatísticas Descritivas para Tempo",
+        col.names=c("Média","Mediana","Desvio Padrão",
+                    "Máximo","Mínimo"))
+
+estatisticas_descritivas_tempo
+
+#histograma
+
+banco_tempo %>%
+  ggplot() +
+  geom_histogram(aes(x=Tempo),
+                 binwidth = 1,
+                 color="black",
+                 fill="orange")
+
+
