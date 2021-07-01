d2118bed918a8e28497c727f24ef172a92ff83a5
aviola98 <viola1@usp.br>
2021-07-01 00:41:54 -0300

    first commit

---
+++ lab_11.py
@@ -0,0 +1,16 @@
+import math
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+
+(999.9-1000)/(250/25)^0.5
+
+#ex 3
+se1=math.sqrt(14*(100-14)/900)
+z1=(15-14)/se1
+se1
+z1
+se2=math.sqrt(12.2*(100-12.2)/900)
+z2=(15-12.2)/se2
+
+
