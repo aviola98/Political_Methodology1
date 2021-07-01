d1d608d5433331824065e05c93b6fa3689cb9184
aviola98 <viola1@usp.br>
2021-07-01 11:01:46 -0300

    s

--- lab_11.py
+++ lab_11.py
@@ -3,6 +3,20 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
+#ex2
+#a
+
+#m=1L -> m=1000mL
+#SD=0,25L SD=250mL
+#m_s=999.90mL
+#N=25
+#standard error
+
+se=math.sqrt(999.9*(1000-999.9)/25)
+z=(1000-999.9)/se
+
+#b
+
 (999.9-1000)/(250/25)^0.5
 
 #ex 3
