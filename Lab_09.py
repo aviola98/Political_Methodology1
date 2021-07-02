9bedc9f6fcd780a92da83939aea53e2f15b5e2ae
aviola98 <viola1@usp.br>
2021-07-02 10:46:46 -0300

    random selection

--- Lab_09.py
+++ Lab_09.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import random 
+import statistics as st
 
 #opening dataframe
 banco_tempo = pd.read_excel("Banco_Lab 08.xlsx",engine="openpyxl")
@@ -7,4 +9,15 @@ banco_tempo = pd.read_excel("Banco_Lab 08.xlsx",engine="openpyxl")
 banco_tempo = banco_tempo.loc[:, ~banco_tempo.columns.str.contains('^Unnamed')]
 
 #randomly selecting 5 answers and storing it in a new dataframe
+
+banco_tempo_sample=banco_tempo.sample(n = 5)
+
+banco_tempo_sample
+
+#pivoting the datatable in order to do the calculations below
+
+
+
+#calculating the mean of the sample
+
 
