ed0f57be59165e6cd81290a91716dcd3207418b3
aviola98 <viola1@usp.br>
2021-07-02 17:30:45 -0300

    c

--- Lab_09.py
+++ Lab_09.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import random 
 import statistics as st
 
@@ -11,13 +12,34 @@ banco_tempo = banco_tempo.loc[:, ~banco_tempo.columns.str.contains('^Unnamed')]
 #randomly selecting 5 answers and storing it in a new dataframe
 
 banco_tempo_sample=banco_tempo.sample(n = 5)
-
-banco_tempo_sample
+#converting the numbers to numeric value in order to do the calculations below
+banco_tempo_sample=banco_tempo_sample.apply(pd.to_numeric)
 
 #pivoting the datatable in order to do the calculations below
 
-
-
 #calculating the mean of the sample
+banco_tempo_sample
+
+df = pd.DataFrame({'ID': ['P2', 'P2', 'P2', 'P2', 'P2'],
+                   'Values': [60,10,45,40,15]
+                   })
+df
+
+#calculate mean
+data=[60,10,45,40,15]
+mean=st.mean(data)
+mean
+#calculate standard deviation
+stdev=st.stdev(data)
+stdev
+
+#calculate the confidence interval of 68% 
+
+def mean_confidence_interval(data, confidence=0.68):
+  a=1.0*np.array(data)
+  n=len(a)
+  m,se=np.mean(a), scipy.stats.sem(a)
+  h=se*scipy.stats.t.ppf((1+confidence) / 2.,n-1)
+  return m, m-h, m+h
 
 
