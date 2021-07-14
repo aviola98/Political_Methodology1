af1550744da34fff857161cbdb175af80559a35c
aviola98 <viola1@usp.br>
2021-07-14 11:43:37 -0300

    scatter plot

--- Lab_12.py
+++ Lab_12.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import statistics as stat 
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 
 #opening the database
@@ -18,5 +18,14 @@ banco_1["%voto-Partido Incumbente (Y)"].std()
 banco_1 = banco_1.drop(0)
 covariancia =banco_1.cov()
 
-#
+#scatter plot
+pib = [1441.000,3740.000,-1465.000,1275.000,2865.000,6596.000,-0.028]
+votos = [4700,54300,53100,23200,48610,46910,51640]
+
+plt.scatter(pib,votos)
+plt.title("Correlation between GDP and Vote")
+plt.xlabel("Votes")
+plt.ylabel("GDP")
+plt.show()
+
 
