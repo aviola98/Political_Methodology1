1c15874e36d8445619c3aa910ac637e6926db0f8
aviola98 <viola1@usp.br>
2021-07-14 11:31:50 -0300

    calculating the covariance

--- Lab_12.py
+++ Lab_12.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import statistics as stat 
 import numpy as np
+
+
+
 #opening the database
 banco_1 = pd.read_excel("Lab_12.xlsx", engine="openpyxl")
 #calulating the mean 
@@ -12,11 +15,8 @@ banco_1["PIB per capita – variação (X)"].std()
 banco_1["%voto-Partido Incumbente (Y)"].std()
 
 #calculating the covariance
-banco_1 = banco_1.drop["Governo"]
-banco_1 = banco_1.drop[0]
-banco_1.groupby("Ano").cov()
+banco_1 = banco_1.drop(0)
+covariancia =banco_1.cov()
 
-cov_mat = np.stack(("PIB per capita – variação (X)","%voto-Partido Incumbente (Y)"))
-np.shape(cov_mat)
-
+#
 
