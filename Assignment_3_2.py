2b51a5d7c1051a157e9330f0f0cd2b771d6b950e
aviola98 <viola1@usp.br>
2021-07-19 11:20:38 -0300

    changing some details regarding the new (adjusted) dataset

--- Assignment_3_2.py
+++ Assignment_3_2.py
@@ -4,8 +4,8 @@ from scipy.stats import chi2_contingency
 data = pd.read_excel("contingency_table.xlsx", engine="openpyxl")
 data
 
-data = data.drop(3)
-data = data.drop('Unnamed: 0', axis=1)
+data = data.drop(5)
+data = data.drop('Regiao', axis=1)
 data
 
 chiVal, piVal, df, exp = chi2_contingency(data)
