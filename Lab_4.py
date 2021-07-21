770ed520b9f2d3976663341cec9b7ae127217875
aviola98 <viola1@usp.br>
2021-07-21 20:11:02 -0300

    getting started by redoing list 4 using python

---
+++ Lab_4.py
@@ -0,0 +1,10 @@
+#redoing lab4 with python
+import pandas as pd
+import statistics as stat 
+import numpy as np
+import matplotlib.pyplot as plt
+
+Banco_Lab4 = pd.read_excel("Banco_Lab 04_novo.xlsx", engine="openpyxl")
+Banco_Lab4
+
+Banco_Lab4.describe
