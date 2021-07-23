49bd17b7662136790b0eca1cce429f2fdc88ac25
aviola98 <viola1@usp.br>
2021-07-23 15:34:33 -0300

    fixing the code

--- Assignment_3_2.py
+++ Assignment_3_2.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from scipy.stats import chi2_contingency
 
-data = pd.read_excel("contingency_table.xlsx", engine="openpyxl")
+data = pd.read_excel("TESTEE.xlsx", engine="openpyxl")
 data
 
-data = data.drop(5)
-data = data.drop('Regiao', axis=1)
+data = data.drop([5])
+data = data.drop(columns = ['Total','Regiões'])
 data
 
 chiVal, piVal, df, exp = chi2_contingency(data)
