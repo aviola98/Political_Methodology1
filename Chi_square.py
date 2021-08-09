8d345b8cf9a5069d31970c04e1e87fcae81328a3
aviola98 <viola1@usp.br>
2021-08-09 15:59:25 -0300

    c

--- Chi_square.py
+++ Chi_square.py
@@ -41,3 +41,47 @@ chiVal, piVal, df, exp = chi2_contingency(df)
 chiVal, piVal, df, exp
 #p-value<5% we can reject the null Hypotesis
 
+#2010
+data = {'Incumbente':[38.7,52.3],
+       'Esp':[45.8,45.2],
+       'Oposição':[56,41],
+       'Esp2':[48.9,48.1]}
+
+
+df = pd.DataFrame(data)
+
+print(df)
+
+chiVal, piVal, df, exp = chi2_contingency(df)
+chiVal, piVal, df, exp
+#p-value<5% we can reject the null hypotesis
+
+#2014
+data = {'Incumbente':[21.9,48.4],
+       'Esp':[35.4,34.9],
+       'Oposição':[73.4,45.3],
+       'Esp2':[59.9,58.8]}
+
+
+df = pd.DataFrame(data)
+
+print(df)
+
+chiVal, piVal, df, exp = chi2_contingency(df)
+chiVal, piVal, df, exp
+#pi-value<5% we can reject the null hypotesis
+
+#2018
+data = {'Incumbente':[26.7,40.6],
+       'Esp':[34,33.3],
+       'Oposição':[65.5,49.8],
+       'Esp2':[58.2,57.1]}
+
+
+df = pd.DataFrame(data)
+
+print(df)
+
+chiVal, piVal, df, exp = chi2_contingency(df)
+chiVal, piVal, df, exp
+#p-value < 5% we can reject the null hypotesis
